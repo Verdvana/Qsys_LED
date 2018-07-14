4a25b7fa8a1b56c3f1fe99fabd0eca29b7afa52b
VERDVANA-LAPTOP\Verdvana <739197909@qq.com>
2018-07-14 22:52:26 +0800

    no message

--- Qsys_LED/Qsys_LED_inst.v
+++
@@ -1,7 +0,0 @@
-	Qsys_LED u0 (
-		.clk_clk                        (<connected-to-clk_clk>),                        //                     clk.clk
-		.key_external_connection_export (<connected-to-key_external_connection_export>), // key_external_connection.export
-		.led_external_connection_export (<connected-to-led_external_connection_export>), // led_external_connection.export
-		.reset_reset_n                  (<connected-to-reset_reset_n>)                   //                   reset.reset_n
-	);
-
